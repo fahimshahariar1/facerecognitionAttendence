7619f7f78cb4dc158d08da2b6b8111bb949ff798
Fahim <fahimshahariar.fs@gmail.com>
2023-11-28 01:33:13 +0600

    2

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ imgStudent = []
 while True:
     success, img = cap.read()
 
-    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.resize(img, (0, 1), None, 0.25, 0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
     faceCurFrame = face_recognition.face_locations(imgS)
